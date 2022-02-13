d1a8c72d420d0588aa60754406dfde3c0056f2dc
MajdHarbb <majdwalidharb@gmail.com>
2022-02-13 21:55:34 +0200

    signup isset

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -1,49 +1,68 @@
 <?php
 
-include("connection.php");
+include("../DB/DBConnection.php");
 
-//$name = $_POST["name"];
-$first_name = "majd";
-$last_name = "harb";
-$email = "majd@harb.com";
-$phone = "03253";
-$gender = "M";
-$dob = date("Y-m-d");
+
+//init variables
+$first_name ="";
+$last_name = "";
+$email = "";
+$phone = "";
+$gender = "";
+$username = "";
+$pass = "";
+$dob = "";
+//get dates
 $date_created = date("Y-m-d");
 $date_updated = date("Y-m-d");
-$username = "majdharb";
-$pass = "dsfsdf";
 
-$insert_user_query = $mysqli->prepare(" INSERT INTO user(username, password, date_created, date_updated) 
-VALUES (?,?,?,?)"); 
-$insert_user_query->bind_param("ssss", $username,$pass,$date_created, $date_updated);
-$insert_user_query->execute();
+//check if values are not null
+if (isset($_POST["first_name"],$_POST["last_name"],$_POST["email"],
+    $_POST["phone"],$_POST["gender"],$_POST["dob"],$_POST["gender"],$_POST["pass"],$_POST["username"];)){
+    
+        $first_name=$_POST["first_name"];
+        $last_name = $_POST["last_name"];
+        $email=$_POST["email"];
+        $username=$_POST["username"];
+        $phone = $_POST["phone"];
+        $gender = $_POST["gender"];
+        $dob = $_POST["dob"];
+        $pass = $_POST["password"];
+      
+     
+        //insert into user table
+        $insert_user_query = $mysqli->prepare("INSERT INTO user(username, password, date_created, date_updated) 
+        VALUES (?,?,?,?)"); 
+        $insert_user_query->bind_param("ssss", $username,$pass,$date_created, $date_updated);
+        $insert_user_query->execute();
 
+        //get user id
+        $query_id = $mysqli->prepare("Select user_id FROM user where username = '".$username."';");
+        $query_id->execute();
 
-$query_id = $mysqli->prepare("Select user_id FROM user where username = '".$username."';");
-$query_id->execute();
+        $id;
+        $array = $query_id->get_result();
+        $array_response = [];
+        while($user = $array->fetch_assoc()){
+            $array_response[] = $user;
+            $id = $user['user_id'];
+        }
+        print_r($id);
+        //$json_response = json_encode($array_response);
+        //echo $json_response;
 
-//$query->store_result();
-//print_r($query->num_rows);
-
-$array = $query_id->get_result();
-$array_response = [];
-while($user = $array->fetch_assoc()){
-    print_r($user['user_id']);
-    $array_response[] = $user;
+        //insert into user_info
+        $insert_user_info_query = $mysqli->prepare("INSERT INTO user_info(user_id,first_name, last_name, email, phone,gender,dob, date_created, date_updated) 
+        VALUES (?,?,?,?,?,?,?,?,?)"); 
+        $insert_user_info_query->bind_param("sssssssss",$id ,$first_name,$last_name,$email,$phone,$gender,$dob,$date_created, $date_updated);
+        $insert_user_info_query->execute();
+       
+       header("location:Login.php");
 }
-$json_response = json_encode($array_response);
-echo $json_response;
-
-
-$insert_user_info_query = $mysqli->prepare(" INSERT INTO user_info(first_name, last_name, email, phone,gender,dob, date_created, date_updated) 
-VALUES (?,?,?,?,?,?,?,?)"); 
-$insert_user_info_query->bind_param("ssssssss", $first_name,$last_name,$email,$phone,$gender,$dob,$date_created, $date_updated);
-
-
-
-$insert_user_info_query->execute();
-
+else {
+    
+     header("location:SignUp.php");
+}
 
 
 ?>
