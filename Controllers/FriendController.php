c3d772b5327f3c5dd7d631a9cdcbcf348d5e945a
MajdHarbb <majdwalidharb@gmail.com>
2022-02-14 13:06:01 +0200

    friend controleler

--- Controllers/FriendController.php
+++ Controllers/FriendController.php
@@ -1,21 +1,67 @@
 <?php
 
-include("../DB/DBConnection.php");
-$id = 64;
-$get_user = $mysqli->prepare("SELECT user_two_id FROM friend WHERE user_one_id = '$id';"); 
-$get_user->execute();
+class FriendController
+{
+    // likes_id	user_id	post_id
 
-//$query->store_result();
-//print_r($query->num_rows);
+    public function create()
+    {
+        include "../DB/DBConnection.php";
+        $user_id = $_POST["user_id"];
+        $friend_id = $_POST["friend_id"];
 
-$array = $get_user->get_result();
+        $query = $mysqli->prepare("INSERT INTO `friend` VALUES (NULL,?,?)");
+        $query->bind_param("ii", $user_id, $friend_id);
+        $query->execute();
+    }
 
-$array_response = [];
-while($user = $array->fetch_assoc()){
-    
-    $array_response[] = $user;
-}
+    //No Update for Likes Table
+    // public function update()
+    // {
+    // }
 
-$json_response = json_encode($array_response);
-echo $json_response;
+    public function delete()
+    {
+        include "../DB/DBConnection.php";
+
+        $user_id = $_POST["user_id"];
+        $friend_id = $_POST["friend_id"];
+
+        $query = $mysqli->prepare("DELETE FROM friend WHERE user_one_id=? AND user_one_id=?");
+        $query->bind_param("ii", $user_id , $friend_id);
+        $query->execute();
+    }
+
+    public function read(){
+        include "../DB/DBConnection.php";
+
+        $query = $mysqli->prepare("");
+        $query->execute();
+        $array = $query->get_result();
+        $array_response = [];
+
+        while ($user_info = $array->fetch_assoc()) {
+            $array_response[] = $user_info;
+        }
+        $json_response = json_encode($array_response);
+        echo $json_response;
+    }
+    public function getFriendReqs()
+    {
+        include "../DB/DBConnection.php";
+
+        $user_id = $_POST["user_id"];
+        $get_user = $mysqli->prepare("SELECT user_two_id FROM friend WHERE user_one_id = '$user_id';"); 
+
+        $query->bind_param("i", $user_id);
+        $query->execute();
+        $array = $query->get_result();
+        $array_response = [];
+
+        while ($user_info = $array->fetch_assoc()) {
+            $array_response[] = $user_info;
+        }
+        $json_response = json_encode($array_response);
+        echo $json_response;
+    }
 ?>
