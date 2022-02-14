7a5310d93112fbba3c0147efe0b0dcca26e9ea90
MajdHarbb <majdwalidharb@gmail.com>
2022-02-14 12:47:35 +0200

    update block

--- Controllers/BlockController.php
+++ Controllers/BlockController.php
@@ -1,21 +1,68 @@
 <?php
 
-include("../DB/DBConnection.php");
-$id = 64;
-$get_user = $mysqli->prepare("SELECT blocked_user_id FROM block WHERE user_id = '$id';"); 
-$get_user->execute();
+class BlockController
+{
+    // likes_id	user_id	post_id
+    public function create()
+    {
+        include "../DB/DBConnection.php";
+        $user_id = $_POST["user_id"];
+        $post_id = $_POST["blocked_user_id"];
 
-//$query->store_result();
-//print_r($query->num_rows);
+        $query = $mysqli->prepare("INSERT INTO `block` VALUES (NULL,?,?)");
+        $query->bind_param("ss", $user_id, $blocked_user_id);
+        $query->execute();
+    }
 
-$array = $get_user->get_result();
+    //No Update for Likes Table
+    // public function update()
+    // {
+    // }
 
-$array_response = [];
-while($user = $array->fetch_assoc()){
-    
-    $array_response[] = $user;
+    public function delete()
+    {
+        include "../DB/DBConnection.php";
+
+        $user_id = $_POST["user_id"];
+        $blocked_user_id = $_POST["blocked_user_id"];
+
+        $query = $mysqli->prepare("DELETE FROM block WHERE user_id=? AND blocked_user_id=?");
+        $query->bind_param("ss", $user_id, $blocked_user_id);
+        $query->execute();
+    }
+
+    public function read(){
+        include "../DB/DBConnection.php";
+
+        $query = $mysqli->prepare("");
+        $query->execute();
+        $array = $query->get_result();
+        $array_response = [];
+
+        while ($user_info = $array->fetch_assoc()) {
+            $array_response[] = $user_info;
+        }
+        $json_response = json_encode($array_response);
+        echo $json_response;
+    }
+    public function getBlockedUsers()
+    {
+        include "../DB/DBConnection.php";
+
+        $user_id = $_POST["user_id"];
+        $query = $mysqli->prepare("SELECT blocked_user_id FROM block WHERE user_id = '$user_id';");
+        $query->bind_param("i", $user_id);
+        $query->execute();
+        $array = $query->get_result();
+        $array_response = [];
+
+        while ($user_info = $array->fetch_assoc()) {
+            $array_response[] = $user_info;
+        }
+        $json_response = json_encode($array_response);
+        echo $json_response;
+    }
 }
 
-$json_response = json_encode($array_response);
-echo $json_response;
+
 ?>
